4c12d318ac7bfe3775766212f79f3e982c27c4ff
Vinh Thinh Ho <hovinhthinh@gmail.com>
2018-12-11 09:42:58 +0100

    Create Quadruple.java

---
+++ util/Quadruple.java
@@ -0,0 +1,23 @@
+package util;
+
+
+public class Quadruple<A, B, C, D> {
+    public A first;
+    public B second;
+    public C third;
+    public D fourth;
+
+    public Quadruple(A first, B second, C third, D fourth) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.fourth = fourth;
+    }
+
+    public Quadruple() {
+        first = null;
+        second = null;
+        third = null;
+        fourth = null;
+    }
+}
