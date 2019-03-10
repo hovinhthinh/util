aec390c09cf3030dc0197b23b5351c146fd06a06
Vinh Thinh Ho <hovinhthinh@gmail.com>
2019-03-10 22:11:44 +0100

    Update CollaborationPriorityQueue.java

--- util/CollaborationPriorityQueue.java
+++ util/CollaborationPriorityQueue.java
@@ -1,24 +1,35 @@
 package util;
 
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 
 // Blocking priority queue, returns null for pop operations only when all collaborators call "pop", otherwise they
 // will be blocked until there is some element pushed into the queue.
 class CollaborationPriorityQueue<T> {
     private final Object lock = new Object();
-    private PriorityQueue<T> queue;
+    private Queue<T> queue;
     private int numberOfCollaborators;
     private int currentWaitCount = 0;
 
     private boolean isEnded = false;
 
+    // comparator == null denotes a normal FIFO queue.
     public CollaborationPriorityQueue(int nCollaborators, Comparator<T> comparator) {
         if (nCollaborators < 1) {
             throw new RuntimeException("nCollaborators must be positive.");
         }
         numberOfCollaborators = nCollaborators;
-        queue = new PriorityQueue<>(comparator);
+        if (comparator == null) {
+            queue = new LinkedList<>();
+        } else {
+            queue = new PriorityQueue<>(comparator);
+        }
+    }
+
+    public CollaborationPriorityQueue(int nCollaborators) {
+        this(nCollaborators, null);
     }
 
     public boolean push(T element) {
